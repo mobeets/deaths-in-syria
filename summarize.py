8bd7f00e83dc4c131d76cc8c95460bf3f7003c11
mobeets <mobeets@gmail.com>
2016-07-25 22:34:56 -0400

    we

--- summarize.py
+++ summarize.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 KEY = '# killed'
-GROUPS = ['Date of death', 'Status']
+GROUPS = ['Date of death']
+# GROUPS = ['Date of death', 'Status']
 # GROUPS = ['Date of death', 'Sex']
 # GROUPS = ['Date of death', 'Area']
 # GROUPS = ['Date of death', 'Province']
@@ -10,9 +11,12 @@ def main(infile='data/deaths-in-syria.csv', outfile='data/counts.csv'):
     df = pd.read_csv(infile)
     df = df.rename(columns={'index': KEY})
     ix = df['Date of death'] > '1970-01-01'
-    # ix = ix & (df['Status'] == 'Civilian')
+    ix = ix & (df['Status'] == 'Civilian')
     # 1/0
-    df[ix].groupby(GROUPS).count()[KEY].unstack().to_csv(outfile, header=True)
+    dff = df[ix].groupby(GROUPS).count()[KEY]
+    if len(GROUPS) > 1:
+        dff = dff.unstack()
+    dff.to_csv(outfile, header=True)
 
 if __name__ == '__main__':
     main()
